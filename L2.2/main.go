c9b41248de73a1b3df502231a3ceaf94cffee797
Michael Sokolskiy <mysokolsky@gmail.com>
2026-01-04 14:27:18 +0300

    === L2.2 == develop === ДАТА 04-01-26 === ВРЕМЯ 14:27:18 =====

--- L2.2/main.go
+++ L2.2/main.go
@@ -1,6 +1,34 @@
+// L2.2
+
+// Задание:
+// Что выведет программа?
+
+// Объяснить порядок выполнения defer функций и итоговый вывод.
+
+// Результат:
+
 package main
 
-func main() {
-println(123)
+import "fmt"
+
+func test() (x int) {
+	defer func() {
+		x++
+	}()
+	x = 1
+	return
 }
 
+func anotherTest() int {
+	var x int
+	defer func() {
+		x++
+	}()
+	x = 1
+	return x
+}
+
+func main() {
+	fmt.Println(test())
+	fmt.Println(anotherTest())
+}
